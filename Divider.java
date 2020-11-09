663eea95c7ad9d0c7665bd1f0f06e276df5e269d
Anel Suljkanovic <anelsuljkanovic04@gmail.com>
2020-11-09 17:40:45 +0100

    divider method erstellt

--- Divider.java
+++ Divider.java
@@ -10,6 +10,10 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,11 +21,10 @@ import java.util.logging.Logger;
  *
  * @author anels
  */
-public class Divider {
+public class Divider{
+    Scanner sca = new Scanner(System.in);
 
-    ArrayList<Integer> liste = new ArrayList<>();
-
-    public ArrayList<Integer> Reader(String filename) throws FileNotFoundException {
+    public ArrayList<Integer> Reader(String filename, ArrayList<Integer> list) throws FileNotFoundException {
         
         FileReader file = new FileReader(filename);
         BufferedReader br = new BufferedReader(file);
@@ -34,7 +37,7 @@ public class Divider {
                 for (String string : line) {
                     try{
                     Integer.parseInt(string);
-                    liste.add(Integer.parseInt(string));
+                    list.add(Integer.parseInt(string));
                     }catch(final NumberFormatException e){
                     
                     }
@@ -44,10 +47,28 @@ public class Divider {
         } catch (IOException ex) {
             Logger.getLogger(Divider.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return liste;
+        return list;
     }
     
-    public void divid(int divider, int chunks){
+    public void divid(List<Integer> list){
+        System.out.println("In wie viele chunks soll die liste geteilt werden");
+        int chunks = Integer.parseInt(sca.nextLine());
+        System.out.println("Welchen Teiler wollen sie verwenden");
+        int divider = Integer.parseInt(sca.nextLine());
+        int[] input = new int[]{chunks, divider};
         
+        while(input.length == 0 || (input[0]<1||(input[1]<1))){
+            
+        }
+        int csize = list.size()/chunks;
+        
+        ThreadPoolExecutor exe = (ThreadPoolExecutor) Executors.newFixedThreadPool(chunks);
+        for (int i = 0; i <= chunks; i++) {
+            int ii = 1;
+            exe.execute(() -> {
+                list.subList(ii * csize, (ii+1)*csize).stream().filter(q->q%divider==0).forEach(System.out::println);
+                        
+            });
+        }exe.shutdown();
     }
 }
